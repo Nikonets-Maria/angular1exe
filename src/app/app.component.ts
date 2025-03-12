aca51cebfdd7e6d20391dcd6547d1f6a371e0233
Nikonets-Maria <maria.nikonec@gmail.com>
2025-03-12 16:45:16 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { LatestProductsComponent } from "./components/latest-products/latest-products.component";
+import { FeaturedProductsComponent } from "./components/featured-products/featured-products.component";
+import { SellersComponent } from "./components/sellers/sellers.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, LatestProductsComponent, FeaturedProductsComponent, SellersComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/sellers/sellers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sellers',
+  imports: [],
+  templateUrl: './sellers.component.html',
+  styleUrl: './sellers.component.css'
+})
+export class SellersComponent {
+
+}
